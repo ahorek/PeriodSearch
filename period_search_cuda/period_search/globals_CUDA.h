0d06996e2acb248f7ef789f55390699d033cbaa4
Pavel Rosický <pavel.rosicky@easy.cz>
2023-11-19 03:09:12 +0100

    x

--- period_search_cuda/period_search/globals_CUDA.h
+++ period_search_cuda/period_search/globals_CUDA.h
@@ -46,7 +46,6 @@ __device__ __device_builtin__ double __hiloint2double(int hi, int lo);
 //#endif
 
 #include "constants.h"
-#include "cudamemasm.h"
 
 //NOTE: https://devtalk.nvidia.com/default/topic/517801/-34-texture-is-not-a-template-34-error-mvs-2010/
 
@@ -135,49 +134,7 @@ __managed__ extern double dev_best[N_BLOCKS];
 __managed__ extern double la_best[N_BLOCKS];
 __managed__ extern double be_best[N_BLOCKS];
 
-
-// vars
-__device__ extern double Dblm[2][3][3][N_BLOCKS]; // OK, set by [tid], read by [bid]
-__device__ extern double Blmat[3][3][N_BLOCKS];   // OK, set by [tid], read by [bid]
-
-__device__ extern double CUDA_scale[N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
-__device__ extern double ge[2][3][N_BLOCKS][POINTS_MAX + 1];     // OK [bid][tid]
-__device__ extern double gde[2][3][3][N_BLOCKS][POINTS_MAX + 1]; // OK [bid][tid]
-__device__ extern double jp_dphp[3][N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
-
-__device__ extern double dave[N_BLOCKS][MAX_N_PAR + 1];
-__device__ extern double atry[N_BLOCKS][MAX_N_PAR + 1];
-
-__device__ extern double chck[N_BLOCKS];
-__device__ extern int    isInvalid[N_BLOCKS];
-__device__ extern int    isNiter[N_BLOCKS];
-__device__ extern int    isAlamda[N_BLOCKS];
-__device__ extern double Alamda[N_BLOCKS];
-__device__ extern int    Niter[N_BLOCKS];
-__device__ extern double iter_diffg[N_BLOCKS];
-__device__ extern double rchisqg[N_BLOCKS]; // not needed
-__device__ extern double dev_oldg[N_BLOCKS];
-__device__ extern double dev_newg[N_BLOCKS];
-
-__device__ extern double trial_chisqg[N_BLOCKS];
-__device__ extern double aveg[N_BLOCKS];
-__device__ extern int    npg[N_BLOCKS];
-__device__ extern int    npg1[N_BLOCKS];
-__device__ extern int    npg2[N_BLOCKS];
-
-__device__ extern double Ochisq[N_BLOCKS];
-__device__ extern double Chisq[N_BLOCKS];
-__device__ extern double Areag[N_BLOCKS][MAX_N_FAC + 1];
-
-
-#ifdef NEWDYTEMP
-__device__ extern double dytemp[POINTS_MAX + 1][40][N_BLOCKS];
-#endif
-
 #define CUDA_Nphpar 3
-
-#define blockIdx() (blockIdx.x + gridDim.x * threadIdx.y)
-
 #define UNRL 4
 #define BLOCKX4 4
 #define BLOCKX8 8
