ef13696c8ad265ceaa5d9100c2e05eea08813329
Pavel Rosický <pavel.rosicky@easy.cz>
2023-11-19 03:54:25 +0100

    x

--- period_search_cuda/period_search/cuda_vars.cuh
+++ period_search_cuda/period_search/cuda_vars.cuh
@@ -1,46 +1,46 @@
 #pragma once
 
 // vars
-__device__ double Dblm[2][3][3][N_BLOCKS]; // OK, set by [tid], read by [bid]
-__device__ double Blmat[3][3][N_BLOCKS];   // OK, set by [tid], read by [bid]
+__device__ extern double Dblm[2][3][3][N_BLOCKS]; // OK, set by [tid], read by [bid]
+__device__ extern double Blmat[3][3][N_BLOCKS];   // OK, set by [tid], read by [bid]
 
-__device__ double CUDA_scale[N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
-__device__ double ge[2][3][N_BLOCKS][POINTS_MAX + 1];     // OK [bid][tid]
-__device__ double gde[2][3][3][N_BLOCKS][POINTS_MAX + 1]; // OK [bid][tid]
-__device__ double jp_dphp[3][N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
+__device__ extern double CUDA_scale[N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
+__device__ extern double ge[2][3][N_BLOCKS][POINTS_MAX + 1];     // OK [bid][tid]
+__device__ extern double gde[2][3][3][N_BLOCKS][POINTS_MAX + 1]; // OK [bid][tid]
+__device__ extern double jp_dphp[3][N_BLOCKS][POINTS_MAX + 1];   // OK [bid][tid]
 
-__device__ double dave[N_BLOCKS][MAX_N_PAR + 1];
-__device__ double atry[N_BLOCKS][MAX_N_PAR + 1];
+__device__ extern double dave[N_BLOCKS][MAX_N_PAR + 1];
+__device__ extern double atry[N_BLOCKS][MAX_N_PAR + 1];
 
-__device__ double chck[N_BLOCKS];
-__device__ int    isInvalid[N_BLOCKS];
-__device__ int    isNiter[N_BLOCKS];
-__device__ int    isAlamda[N_BLOCKS];
-__device__ double Alamda[N_BLOCKS];
-__device__ int    Niter[N_BLOCKS];
-__device__ double iter_diffg[N_BLOCKS];
-__device__ double rchisqg[N_BLOCKS]; // not needed
-__device__ double dev_oldg[N_BLOCKS];
-__device__ double dev_newg[N_BLOCKS];
+__device__ extern double chck[N_BLOCKS];
+__device__ extern int    isInvalid[N_BLOCKS];
+__device__ extern int    isNiter[N_BLOCKS];
+__device__ extern int    isAlamda[N_BLOCKS];
+__device__ extern double Alamda[N_BLOCKS];
+__device__ extern int    Niter[N_BLOCKS];
+__device__ extern double iter_diffg[N_BLOCKS];
+__device__ extern double rchisqg[N_BLOCKS]; // not needed
+__device__ extern double dev_oldg[N_BLOCKS];
+__device__ extern double dev_newg[N_BLOCKS];
 
-__device__ double trial_chisqg[N_BLOCKS];
-__device__ double aveg[N_BLOCKS];
-__device__ int    npg[N_BLOCKS];
-__device__ int    npg1[N_BLOCKS];
-__device__ int    npg2[N_BLOCKS];
+__device__ extern double trial_chisqg[N_BLOCKS];
+__device__ extern double aveg[N_BLOCKS];
+__device__ extern int    npg[N_BLOCKS];
+__device__ extern int    npg1[N_BLOCKS];
+__device__ extern int    npg2[N_BLOCKS];
 
-__device__ double Ochisq[N_BLOCKS];
-__device__ double Chisq[N_BLOCKS];
-__device__ double Areag[N_BLOCKS][MAX_N_FAC + 1];
+__device__ extern double Ochisq[N_BLOCKS];
+__device__ extern double Chisq[N_BLOCKS];
+__device__ extern double Areag[N_BLOCKS][MAX_N_FAC + 1];
 
 //LFR
-__managed__ int isReported[N_BLOCKS];
-__managed__ double dark_best[N_BLOCKS];
-__managed__ double per_best[N_BLOCKS];
-__managed__ double dev_best[N_BLOCKS];
-__managed__ double la_best[N_BLOCKS];
-__managed__ double be_best[N_BLOCKS];
+__managed__ extern int isReported[N_BLOCKS];
+__managed__ extern double dark_best[N_BLOCKS];
+__managed__ extern double per_best[N_BLOCKS];
+__managed__ extern double dev_best[N_BLOCKS];
+__managed__ extern double la_best[N_BLOCKS];
+__managed__ extern double be_best[N_BLOCKS];
 
 #ifdef NEWDYTEMP
-__device__ double dytemp[POINTS_MAX + 1][40][N_BLOCKS];
+__device__ extern double dytemp[POINTS_MAX + 1][40][N_BLOCKS];
 #endif
