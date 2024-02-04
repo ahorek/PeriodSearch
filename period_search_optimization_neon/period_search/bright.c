10ee8023ce2c10f16830a8baa3139ba444cf8d8a
Pavel Rosický <pavel.rosicky@easy.cz>
2024-02-04 22:14:49 +0100

    x

--- period_search_optimization_neon/period_search/bright.c
+++ period_search_optimization_neon/period_search/bright.c
@@ -216,10 +216,14 @@ double bright(double ee[], double ee0[], double t, double cg[], double dyda[], i
 	  else if (icmp & 2)
 	  {
  		 INNER_CALC_DSMU
-         avx_pbr = vextq_f64(avx_pbr, vdupq_n_f64(0.0), 1);
-         avx_dsmu = vextq_f64(vdupq_n_f64(0.0), avx_dsmu, 1);
-         avx_dsmu0 = vextq_f64(vdupq_n_f64(0.0), avx_dsmu0, 1);
-         avx_lmu = vextq_f64(vdupq_n_f64(0.0), avx_lmu, 1);
+         //avx_pbr = vextq_f64(avx_pbr, vdupq_n_f64(0.0), 1);
+         //avx_dsmu = vextq_f64(vdupq_n_f64(0.0), avx_dsmu, 1);
+         //avx_dsmu0 = vextq_f64(vdupq_n_f64(0.0), avx_dsmu0, 1);
+         //avx_lmu = vextq_f64(vdupq_n_f64(0.0), avx_lmu, 1);
+         avx_pbr = vcombine_f64(vget_high_f64(avx_pbr), vdup_n_f64(0.0));
+         avx_dsmu = vcombine_f64(vget_high_f64(avx_dsmu), vdup_n_f64(0.0));
+         avx_dsmu0 = vcombine_f64(vget_high_f64(avx_dsmu0), vdup_n_f64(0.0));
+         avx_lmu = vcombine_f64(vget_high_f64(avx_lmu), vdup_n_f64(0.0));
          avx_lmu0 = vcombine_f64(vget_low_f64(avx_11), vget_high_f64(avx_lmu0));
 
          printVec(avx_pbr);
@@ -227,6 +231,7 @@ double bright(double ee[], double ee0[], double t, double cg[], double dyda[], i
          printVec(avx_dsmu0);
          printVec(avx_lmu);
          printVec(avx_lmu0);
+         printf("\n");
 
          Dg_row[incl_count] = (float64x2_t*)&Dg[i + 1];
 
