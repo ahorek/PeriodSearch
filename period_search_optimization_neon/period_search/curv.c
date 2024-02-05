7cf62d3f9ee8d29de9c55950e8ee24da06696a7d
Pavel Rosický <pavel.rosicky@easy.cz>
2024-02-05 02:11:57 +0100

    x

--- period_search_optimization_neon/period_search/curv.c
+++ period_search_optimization_neon/period_search/curv.c
@@ -6,6 +6,7 @@
 #include <cmath>
 #include "globals.h"
 #include "constants.h"
+#include <arm_neon.h>
 
 void curv(double cg[])
 {
@@ -39,5 +40,13 @@ void curv(double cg[])
 
       for (k = 1; k <= n; k++)
          Dg[i-1][k-1] = g * Dsph[i][k];
+
+      //float64x2_t avx_g = vdupq_n_f64(g);
+      //for (k = 1; k < n; k += 2) {
+      //  float64x2_t avx_pom = vld1q_f64(&Dsph[i][k]);
+      //  avx_pom = vmulq_f64(avx_pom, avx_g);
+      //  vst1q_f64(&Dg[i-1][k-1], avx_pom);
+      //}
+      //if (k==n) Dg[i-1][k-1] = g * Dsph[i][k]; //last odd value
    }
 }
