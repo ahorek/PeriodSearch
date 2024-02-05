7eef5645f4892c8cad267893d0dc5b17dc8d2cab
Pavel Rosický <pavel.rosicky@easy.cz>
2024-02-05 01:58:39 +0100

    x

--- period_search_optimization_neon/period_search/bright.c
+++ period_search_optimization_neon/period_search/bright.c
@@ -22,7 +22,6 @@ void printVec(float64x2_t vec)
 
 #define INNER_CALC \
 	res_br = vaddq_f64(res_br, avx_pbr); \
-   printVec(res_br); \
 	float64x2_t avx_sum1, avx_sum10, avx_sum2, avx_sum20, avx_sum3, avx_sum30; \
 	\
 	avx_sum1 = vmulq_f64(avx_Nor1, avx_de11); \
@@ -246,8 +245,6 @@ double bright(double ee[], double ee0[], double t, double cg[], double dyda[], i
    Dg_row[incl_count + 2] = Dg_row[0];
    Dg_row[incl_count + 3] = Dg_row[0];
 
-   printf("total\n");
-   printVec(res_br);
    res_br = vpaddq_f64(res_br, res_br);
    vst1q_lane_f64(&br, res_br, 0);
 
