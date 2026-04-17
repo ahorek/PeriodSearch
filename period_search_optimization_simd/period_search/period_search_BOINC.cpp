e94ec6e7bfa460a8a59596aa843a19dedbfa6856
Pavel Rosický <pavel.rosicky@easy.cz>
2026-04-17 02:53:00 +0200

    fake

--- period_search_optimization_simd/period_search/period_search_BOINC.cpp
+++ period_search_optimization_simd/period_search/period_search_BOINC.cpp
@@ -58,7 +58,7 @@
 #include "globals.h"
 
 #ifdef _WIN32
-#include "boinc_win.h"
+//#include "boinc_win.h"
 #include <shlwapi.h>
 #include "winbase.h"
 #else
@@ -80,7 +80,7 @@
 #include "str_util.h"
 #include "util.h"
 #include "filesys.h"
-#include "boinc_api.h"
+//#include "boinc_api.h"
 #include "mfile.h"
 #include "arrayHelpers.hpp"
 #include "systeminfo.h"
@@ -124,6 +124,7 @@ constexpr auto output_filename = "period_search_out";
 
 int DoCheckpoint(MFILE& mf, const int nlines, const int newconw, const double conwr, const double sumdarkfacet, const int testperiods)
 {
+    /*
     std::string resolvedName;
 
     const auto file = fopen("temp", "w");
@@ -136,6 +137,7 @@ int DoCheckpoint(MFILE& mf, const int nlines, const int newconw, const double co
     boinc_resolve_filename_s(checkpoint_file, resolvedName);
 	retval = boinc_rename("temp", resolvedName.c_str());
 	if (retval) return retval;
+    */
 
     return 0;
 }
@@ -213,6 +215,58 @@ std::vector<double> da;
 //	}
 //}
 
+
+#include <cstring>
+#include <iostream>
+
+#define BOINC_SUCCESS		0
+#define EXIT_CHILD_FAILED	1
+
+struct BOINC_OPTIONS
+{
+	int normal_thread_priority;
+	int main_program;
+	int check_heartbeat;
+	int handle_process_control;
+	int send_status_msgs;
+	int direct_process_action;
+	int multi_thread;
+	int multi_process;
+};
+
+inline void boinc_options_defaults(BOINC_OPTIONS &) {}
+inline int boinc_init_options(BOINC_OPTIONS *) { std::cout << "boinc_init()" << std::endl; return 0; }
+inline int boinc_finish(const int status) { std::cout << "boinc_finish(" << status << ")" << std::endl; exit(status); /* never reached */ return 0; }
+
+inline int boinc_resolve_filename(const char * const virtual_name, char * const physical_name, const int len)
+{
+	strncpy(physical_name, virtual_name, size_t(len - 1));
+	return 0;
+}
+
+inline FILE * boinc_fopen(const char * const path, const char * const mode)
+{
+	return std::fopen(path, mode);
+}
+
+inline int boinc_time_to_checkpoint() { static int cnt = 0; if (++cnt == 20) { cnt = 0; return 1; } return 0; }
+inline int boinc_checkpoint_completed() { return 0; }
+
+inline int boinc_fraction_done(const double f) { std::cout << "boinc_fraction_done(" << f << ")" << std::endl; return 0; }
+
+struct BOINC_STATUS { int no_heartbeat, suspended, quit_request, abort_request; };
+
+inline int boinc_get_status(BOINC_STATUS * const status)
+{
+	// std::cout << "boinc_get_status" << std::endl;
+	status->no_heartbeat = status->suspended = status->quit_request = status->abort_request = 0;
+	static int cnt = 0;
+	if ((++cnt >= 10) && (cnt < 20)) status->suspended = 1;
+	if (cnt >= 1000) status->suspended = status->abort_request = 1;
+	return 0;
+}
+
+
 int main(int argc, char** argv)
 {
     printf("start");
@@ -253,12 +307,12 @@ int main(int argc, char** argv)
 
     printf("preboinc");
 
-    int retval = boinc_init();
-    if (retval)
-    {
-        fprintf(stderr, "%s boinc_init returned %d\n", boinc_msg_prefix(buf, sizeof(buf)), retval);
-        std::exit(retval);
-    }
+    //int retval = boinc_init();
+    //if (retval)
+    //{
+    //    fprintf(stderr, "%s boinc_init returned %d\n", boinc_msg_prefix(buf, sizeof(buf)), retval);
+    //    std::exit(retval);
+    //}
 
     printf("after boinc");
 
