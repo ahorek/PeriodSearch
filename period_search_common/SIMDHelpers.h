1d9f53217ebab30e8c3ffb875e3818c174110f7b
Pavel Rosický <pdahorek@seznam.cz>
2026-09-03 02:53:49 +0200

    Use posix_memalign for memory allocation
    Replace std::aligned_alloc with posix_memalign for alignment.

--- period_search_common/SIMDHelpers.h
+++ period_search_common/SIMDHelpers.h
@@ -29,7 +29,10 @@ struct AlignedAllocator
     T* allocate(const std::size_t n)
     {
 #if defined __GNUC__ && !defined _WIN32
-        void* ptr = std::aligned_alloc(alignment, n * sizeof(T));
+        void* ptr = nullptr;
+        if (posix_memalign(&ptr, alignment, n * sizeof(T)) != 0) {
+            throw std::bad_alloc();
+        }
 #else
         void* ptr = _aligned_malloc(n * sizeof(T), alignment);
 #endif
